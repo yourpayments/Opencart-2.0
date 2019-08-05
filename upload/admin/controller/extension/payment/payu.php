8d2e5aead2c6de1fa881f37e91407d7c11b1e134
PayU Russia <integration@payu.ru>
2019-08-05 17:20:04 +0300

    Update payu.php

--- upload/admin/controller/extension/payment/payu.php
+++ upload/admin/controller/extension/payment/payu.php
@@ -77,11 +77,8 @@ class ControllerExtensionPaymentPayu extends Controller {
 
 //------------------------------------------------------------
 
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['column_right'] = $this->load->controller('common/column_right');
-		$data['content_top'] = $this->load->controller('common/content_top');
-		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['footer'] = $this->load->controller('common/footer');
 				
 		$this->response->setOutput($this->load->view('extension/payment/payu', $data));
